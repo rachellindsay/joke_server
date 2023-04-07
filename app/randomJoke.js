c99f011d6a6c5bd959c4886cf29f7ebfa66ae7e2
Rachel Lindsay <racheljlindsay@gmail.com>
2023-04-07 09:56:22 -0700

    fixed app.js to end connection and catch errors

--- app/randomJoke.js
+++ app/randomJoke.js
@@ -10,11 +10,20 @@ const pool = mariadb.createPool({
 });
 
 const randomJoke = async function randomJoke() {
-  const conn = await pool.getConnection();
-  const result = await conn.query(
-    "SELECT joke FROM jokes ORDER BY RAND() LIMIT 1"
-  );
-  return result[0];
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const result = await conn.query(
+      "SELECT joke FROM jokes ORDER BY RAND() LIMIT 1"
+    );
+    return result[0];
+  } catch (error) {
+    throw error;
+  } finally {
+    if (conn) {
+      conn.release();
+    }
+  }
 };
 
 module.exports = randomJoke;
