8b05e3bede3ced8b9b3251ee0aba8e499d831d66
Rachel Lindsay <racheljlindsay@gmail.com>
2023-11-29 17:59:25 -0800

    didn't work

--- docker_containers/frontend/src/components/buttons/LoginButton.vue
+++ docker_containers/frontend/src/components/buttons/LoginButton.vue
@@ -8,10 +8,6 @@ import { useAuth0 } from "@auth0/auth0-vue";
 const { loginWithRedirect } = useAuth0();
 
 const handleLogin = () => {
-  loginWithRedirect({
-    loginParams: {
-      returnTo: window.location.reload(),
-    },
-  });
+  loginWithRedirect();
 };
 </script>
