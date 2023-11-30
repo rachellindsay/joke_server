2d49ff829c3196886d6d0f673c67fdf9a468a853
Rachel Lindsay <racheljlindsay@gmail.com>
2023-11-29 17:53:50 -0800

    modified login page trying to get main page to reload after login

--- docker_containers/frontend/src/components/buttons/LoginButton.vue
+++ docker_containers/frontend/src/components/buttons/LoginButton.vue
@@ -8,6 +8,10 @@ import { useAuth0 } from "@auth0/auth0-vue";
 const { loginWithRedirect } = useAuth0();
 
 const handleLogin = () => {
-  loginWithRedirect();
+  loginWithRedirect({
+    loginParams: {
+      returnTo: window.location.reload(),
+    },
+  });
 };
 </script>
